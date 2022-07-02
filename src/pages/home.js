ae762b505d57c478e93268c526b9ee888335e9af
Xuan Tang <xuantang@andrew.cmu.edu>
2022-07-02 17:15:56 -0500

    delete .github folder and add education templates

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,9 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
+
+import education1 from '../templates/education_1.jpg';
+import education2 from '../templates/education_2.jpg';
+import education3 from '../templates/education_3.jpg';
+import education4 from '../templates/education_4.jpg';
 
 const Home = () => {
   return (
@@ -14,6 +18,37 @@ const Home = () => {
       <div className="education">
         <p>Theme - Education</p>
       </div>
+
+      <div className="education-template-block1">
+        <div className="edcation-template1">
+          <img src={education1} alt="Education Template 1" />
+          <button type="button">Select</button>
+        </div>
+        <div className="edcation-template2">
+          <img src={education2} alt="Education Template 2" />
+          <button type="button">Select</button>
+        </div>
+        <div className="edcation-template3">
+          <img src={education3} alt="Education Template 3" />
+          <button type="button">Select</button>
+        </div>
+      </div>
+
+      <div className="education-template-block2">
+        <div className="edcation-template4">
+          <img src={education4} alt="Education Template 4" />
+          <button type="button">Select</button>
+        </div>
+        <div className="edcation-template5">
+          <img src={education4} alt="Education Template 5" />
+          <button type="button">Select</button>
+        </div>
+        <div className="edcation-template6">
+          <img src={education4} alt="Education Template 6" />
+          <button type="button">Select</button>
+        </div>
+      </div>
+
       <div className="technology">
         <p>Theme - Technology</p>
       </div>
@@ -26,9 +61,6 @@ const Home = () => {
       <div className="environment">
         <p>Theme - Environment</p>
       </div>
-      {/* <div className="Button">
-        <button type="button">Select</button>
-      </div> */}
     </>
   );
 };
